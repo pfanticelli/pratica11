ea09d60430cdbee5080eafc4a6eeb5c030eea1a2
pfanticelli <149827618+pfanticelli@users.noreply.github.com>
2023-11-03 19:15:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt-sync')();
+
+idade = parseInt(prompt("Digite sua idade: "));
+tempo = parseInt(prompt("Digite seu tempo de serviço: "));
+salario = parseFloat(prompt("Digite seu salário: "));
+
+// Implementar o comando de decisão para verificar se o cidadão pode, ou não, se aposentar
+if ((idade >= 65) || (tempo >= 30) || (idade >= 60 && tempo >= 25)) {
+
+  // Implementar os comandos de decisão aninhados para calcular o salário de aposentadoria (80% ou 60%)
+  if (tempo > 20) {
+    var aposentadoria = (salario * 80) / 100
+  } else if (tempo <= 20) {
+    var aposentadoria = (salario * 60) / 100
+  }
+  // Implementar os comandos de decisão aninhados para garantir o salário de aposentadoria dentro dos limites inferior (R$ 1212,00) e superior (R$ 7087,22)
+  if (aposentadoria < 1212) {
+    aposentadoria = 1212
+  } else if (aposentadoria > 7087.22) {
+    aposentadoria = 7087.22
+  }
+
+  console.log("Você pode se aposentar. Seu salário de aposentadoria será de R$ " + aposentadoria.toFixed(2))
+} else {
+  console.log("Você ainda não pode se aposentar.")
+}
